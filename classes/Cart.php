889e5b26bcdd0acfd644f04d34dbf06c1cd31157
UK ONY <georgegoldmanjohn.o@gmail.com>
2020-11-29 14:29:02 +0100

    check item if existing

--- classes/Cart.php
+++ classes/Cart.php
@@ -9,7 +9,15 @@ class Cart
     {
         foreach ($this->items as $item)
         {
-            
+            if($item->getProduct()->getId() === $product->getId() )
+            {
+                if($item->getQuantity() + $qautity > $product->getProduct()->getAvailableQuantity())
+                {
+                    throw new Exception("product qauntity can not be more than".$product->getAvailableQuantity());
+                }
+
+                $item->setQuantity($item->getQuantity() + $qautity);
+            }
         }
         $cartItem  = new CartItem($product, $qautity);
     }
--- classes/Product.php
+++ classes/Product.php
@@ -7,6 +7,53 @@ class Product
     private float $price;
     private int $availablePrice;
 
+    public function __construct($id, $title, $price, $availablePrice)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->availablePrice = $availablePrice;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId()
+    {
+        $this->id = $id;
+    }
+
+    public function getTtitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle()
+    {
+        $this->title = $title;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice()
+    {
+        $this->price = $price;
+    }
+
+    public function getAvailablePrice()
+    {
+        return $this->availablePrice;
+    }
+
+    public function setAvailablePrice()
+    {
+        $this->availablePrice = $availablePrice;
+    }
 
     public function addToCart(Cart $cart, int $qauntity)
     {
